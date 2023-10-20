bafb988c2845f65dd7a387a96e0361a9c04f2bc1
Kayce Basques <kayce@google.com>
2023-10-20 08:46:57 -0700

    fix: Refactor class names

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import mbedmgr
+import json
 
 sitemap_url = 'https://pigweed.dev/sitemap.xml'
 embeddings_manager = mbedmgr.EmbeddingsManager()
-print(embeddings_manager.scrape_sitemap(sitemap_url))
+urls = embeddings_manager.scrape_sitemap(sitemap_url)
+data = embeddings_manager.scrape_urls(urls)
+print(json.dumps(data, indent=4))
