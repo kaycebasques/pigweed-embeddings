e230c68787c3125f8484fce98774d0d6b1e1fed1
Kayce Basques <kayce@google.com>
2023-10-23 15:20:50 -0700

    feat: Use minification API

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import mbedmgr
 import json
+import bs4
 
 manager = mbedmgr.EmbeddingsManager()
+
 docs_site = manager.add_website_source(source_id='pigweed_dev')
-docs_site.scrape_sitemap('https://pigweed.dev/sitemap.xml')
-docs_site.scrape_urls()
+docs_site.get_pages_from_sitemap('https://pigweed.dev/sitemap.xml')
+docs_site.scrape_pages()
+docs_site.minify_pages()
+docs_site.save()
