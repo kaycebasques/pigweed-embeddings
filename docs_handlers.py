c08908e56d9d47c5396e5f9112cbc30b1307f328
Kayce Basques <kayce@google.com>
2023-11-11 10:08:35 -0800

    fix: Inherit BaseSource

--- docs_handlers.py
+++ docs_handlers.py
@@ -6,39 +6,37 @@ import openai
 import database
 import utilities
 
-def preprocess(url, mgr):
-    old_page_text = mgr.get_page_text(url)
-    soup = bs4.BeautifulSoup(old_page_text, 'html.parser')
+def preprocess(mgr, url):
+    old_text = mgr.get_text(url)
+    soup = bs4.BeautifulSoup(old_text, 'html.parser')
     main = soup.select('div.main')[0]
     for tag in ['script', 'style', 'link']:
         for useless_node in main.find_all(tag):
             useless_node.decompose()
-    new_page_text = str(main)
-    mgr.set_page_text(url, new_page_text)
+    new_text = str(main)
+    mgr.set_text(url, new_text)
 
-def segment(page_url, mgr):
-    page_text = mgr.get_page_text(page_url)
-    soup = bs4.BeautifulSoup(page_text, 'html.parser')
+def segment(mgr, url):
+    text = mgr.get_text(page_url)
+    soup = bs4.BeautifulSoup(text, 'html.parser')
     for section in soup.find_all('section'):
-        segment = str(section)
-        section_id = section.get('id')
-        if not section_id:
+        segment_text = str(section)
+        segment_id = section.get('id')
+        if not segment_id:
             continue
-        segment_url = f'{page_url}#{section_id}'
+        segment_url = f'{url}#{segment_id}'
         mgr.set_segment(segment_url, segment)
 
-def embed(url, text, checksums):
+def embed(mgr, url, text, checksums):
     openai_client = openai.OpenAI(api_key=os.environ.get('OPENAI_KEY'))
-    max_token_count = 8191
-    model = 'text-embedding-ada-002'
     db = database.Database()
-    if utilities.token_count(text) > max_token_count:
+    if utilities.token_count(text) > utilities.max_token_count():
         return
     if db.row_exists(content=text, checksums=checksums):
         db.update_timestamp(content=text)
     else:
         embedding = openai_client.embeddings.create(
             input=text,
-            model='text-embedding-ada-002'
+            model=utilities.embedding_model()
         ).data[0].embedding
         db.add(content=text, content_type='web', url=url, embedding=embedding)
--- main.py
+++ main.py
@@ -5,9 +5,8 @@ import bs4
 import mbedmgr
 import dotenv
 
-import docs_manager
+import docs_handlers
 import github_handlers
-import bugs_manager
 import database
 
 def main():
@@ -21,18 +20,17 @@ def main():
     manager = mbedmgr.EmbeddingsManager(checksums)
 
     # Official docs manager
-    # docs_site = manager.add_website_source(source_id='pigweed.dev')
-    # docs_site.set_sitemap('https://pigweed.dev/sitemap.xml')
-    # docs_site.scrape_pages_from_sitemap()
-    # docs_site.set_preprocess_handler(docs_manager.preprocess)
-    # docs_site.set_segment_handler(docs_manager.segment)
-    # docs_site.set_embed_handler(docs_manager.embed)
+    docs = manager.add_website_source(source_id='pigweed.dev')
+    docs.scrape_urls_from_sitemap('https://pigweed.dev/sitemap.xml')
+    docs.set_preprocess_handler(docs_handlers.preprocess)
+    docs.set_segment_handler(docs_handlers.segment)
+    docs.set_embed_handler(docs_handlers.embed)
 
     # GitHub source code manager
     include = ['pw_string/*.h']
     exclude = ['third_party/*']
-    github_manager = manager.add_github_source('google', 'pigweed', 'main', include, exclude)
-    github_manager.set_embed_handler(github_handlers.embed)
+    github = manager.add_github_source('google', 'pigweed', 'main', include, exclude)
+    github.set_embed_handler(github_handlers.embed)
 
     # Generate embeddings for all the sources!
     manager.generate()
