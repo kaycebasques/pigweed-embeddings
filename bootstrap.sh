19797a3c2feb233dd7bb434337c430b839b0f795
Kayce Basques <kayce@google.com>
2023-11-11 14:13:30 -0800

    feat: Check for and copy .env

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,14 @@
+if [ ! -f "src/embeddings/.env" ]; then
+    echo "ERROR: //src/embeddings/.env not found"
+    echo "1. Create //src/embeddings/.env"
+    echo "2. Set OPENAI_KEY to OpenAI API Key"
+    echo "3. Set SUPABASE_KEY to service_role Project API Key"
+    echo "4. Set SUPABASE_URL to Project URL"
+    echo "Note: this script copies //src/embeddings/.env to //src/server/functions/.env"
+    exit 1
+fi
+
+cp src/embeddings/.env src/server/functions/.env
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt
