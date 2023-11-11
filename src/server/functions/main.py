c59321d1886bcda37d63ab48e15abd27530a7cec
Kayce Basques <kayce@google.com>
2023-11-11 14:13:51 -0800

    feat: Add Supabase and OpenAI

--- src/server/functions/main.py
+++ src/server/functions/main.py
@@ -1,11 +1,25 @@
+from dotenv import load_dotenv
 from firebase_functions import https_fn, options
 from flask import Flask, request
 from flask_cors import CORS
+from openai import OpenAI
+from supabase import create_client
 
+import openai
+import dotenv
+import supabase
+
+# Init.
+load_dotenv()
+database = supabase.create_client(
+    supabase_url=os.environ.get('SUPABASE_URL'),
+    supabase_key=os.environ.get('SUPABASE_KEY')
+)
+openai = OpenAI(api_key=os.environ.get('OPENAI_KEY'))
 app = Flask(__name__)
 CORS(app)
 
-@app.post('/context')
+@app.get('/context')  # DEBUG: Change back to post
 def context():
     data = request.get_json()
     return 'Hello, world!'
