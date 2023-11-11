d8930feec5220d7e4eff065a32cb23c3734e0c36
Kayce Basques <kayce@google.com>
2023-11-11 14:29:38 -0800

    fix: Rejigger bootstrap stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+# Make sure that env vars file exists.
 if [ ! -f "src/embeddings/.env" ]; then
     echo "ERROR: //src/embeddings/.env not found"
     echo "1. Create //src/embeddings/.env"
@@ -5,11 +6,19 @@ if [ ! -f "src/embeddings/.env" ]; then
     echo "3. Set SUPABASE_KEY to service_role Project API Key"
     echo "4. Set SUPABASE_URL to Project URL"
     echo "Note: this script copies //src/embeddings/.env to //src/server/functions/.env"
-    exit 1
+    return
 fi
-
+# TODO: Check that all env vars exist.
 cp src/embeddings/.env src/server/functions/.env
-python3 -m venv venv
-source venv/bin/activate
-python3 -m pip install -r requirements.txt
+
+# Set up virtual env for generating embeddings.
+python3 -m venv src/embeddings/venv
+source src/embeddings/venv/bin/activate
+python3 -m pip install -r src/embeddings/requirements.txt
+deactivate
+
+# Set up virtual env for running the server.
+python3 -m venv src/server/functions/venv
+source src/server/functions/venv/bin/activate
+python3 -m pip install -r src/server/functions/requirements.txt
 deactivate
